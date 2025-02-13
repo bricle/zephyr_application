a6a6c6a4d13b80fa9560f8daf4c0f41e7677e59e
bricle <bricle031@gmail.com>
2025-02-13 10:43:32 +0800

    spi test

---
+++ spi_bme280/src/main.c
@@ -0,0 +1,12 @@
+#include <sys/_intsup.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+int main(void) {
+
+    return 0;
+}
