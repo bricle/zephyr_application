a4e0cc3a19cf1693a20d072858361af93f346d69
bricle <bricle031@gmail.com>
2025-02-20 09:34:22 +0800

    Add ADC sequence example to sample multiple times ant one read operation

--- adc_sequence/src/main.c
+++ adc_sequence/src/main.c
@@ -1,8 +1,10 @@
+#include <stdint.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/adc.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/logging/log.h>
+#include "zephyr/sys/printk.h"
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 const struct adc_dt_spec adc_channel = ADC_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 0);
@@ -10,10 +12,17 @@ const struct adc_dt_spec adc_channel = ADC_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_use
 int main(void) {
     int err;
     uint32_t count = 0;
-    int16_t buffer;
+    int16_t buffer[5];
+    const struct adc_sequence_options options = {
+        .extra_samplings = 5 - 1,
+        // crucial, extra_samplings field must be set to the total number of samplings - 1
+        .interval_us = 0,
+    };
     struct adc_sequence sequence = {
-        .buffer      = &buffer,
+        .buffer      = buffer,
         .buffer_size = sizeof(buffer),
+        .channels    = BIT(0),
+        .options     = &options,
 
     };
     if (!adc_is_ready_dt(&adc_channel)) {
@@ -38,13 +47,17 @@ int main(void) {
             continue;
         }
         // val_mv = *(int*)sequence.buffer;
-        val_mv = buffer;
-        err = adc_raw_to_millivolts_dt(&adc_channel, &val_mv);
-        if (err < 0) {
-            LOG_ERR("ADC raw to millivolts error %d", err);
-            continue;
+        for (uint32_t i = 0; i < 5; i++) {
+            val_mv = buffer[i];
+            err    = adc_raw_to_millivolts_dt(&adc_channel, &val_mv);
+            if (err < 0) {
+                LOG_ERR("ADC raw to millivolts error %d", err);
+                continue;
+            }
+            printk(" = %d mV", val_mv);
         }
-        LOG_INF(" = %d mV", val_mv);
+        printk("\n\r**********************\n\r");
+        LOG_HEXDUMP_INF(buffer, sizeof(buffer), "adc buffer");
         k_msleep(1000);
     }
 }
