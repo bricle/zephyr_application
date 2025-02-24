24200dfb562c97bf18b0014f5b672037febdd961
bricle <bricle031@gmail.com>
2025-02-24 14:01:48 +0800

    配置lis3dh为中断模式，添加custom dts binding

--- spi_lis3dh/src/main.c
+++ spi_lis3dh/src/main.c
@@ -5,15 +5,16 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/spi.h>
+#include <lis3dh_driver.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 // 模式00和模式11是最常用的两种SPI模式，模式00是时钟空闲时为低电平，数据采样时钟上升沿，模式11是时钟空闲时为高电平，数据采样时钟下降沿。
 // 并且这两种模式读出数据一致，都可用。
 #define SPIOP SPI_WORD_SET(8) | SPI_TRANSFER_MSB | SPI_MODE_CPOL | SPI_MODE_CPHA
 
-struct spi_dt_spec spispec = SPI_DT_SPEC_GET(DT_NODELABEL(bme280), SPIOP, 0);
+struct spi_dt_spec spispec = SPI_DT_SPEC_GET(DT_NODELABEL(lis3dh), SPIOP, 0);
 
-static int bme_write_reg(uint8_t reg, uint8_t value) {
+static int lis3dh_write_reg(uint8_t reg, uint8_t value) {
     int err;
 
     uint8_t tx_buf[]                  = {(reg & 0x7F), value};
@@ -29,7 +30,7 @@ static int bme_write_reg(uint8_t reg, uint8_t value) {
     return 0;
 }
 
-static int bme_read_reg(uint8_t reg, uint8_t* data, uint8_t size) {
+static int lis3dh_read_reg(uint8_t reg, uint8_t* data, uint8_t size) {
     int err;
 
     uint8_t tx_buffer                 = reg | 0x80;
@@ -55,8 +56,20 @@ int main(void) {
     }
     uint8_t data[2] = {0};
     // 重点，要读2个字节，第一个字节是dummy byte，第二个字节才是真正的数据
-    bme_read_reg(0x0f, data, 2);
-    LOG_INF("Chip ID: 0x%x", data[1]);
-    LOG_HEXDUMP_INF(data, sizeof(data), "2regs");
+    lis3dh_read_reg(LIS3DH_WHO_AM_I, data, 2);
+
+    lis3dh_write_reg(LIS3DH_CTRL_REG1, 0x2f); //50HZ,低功耗模式
+    lis3dh_write_reg(LIS3DH_CTRL_REG2, 0x09); //使用高通滤波器，过滤初始加速度
+    lis3dh_write_reg(LIS3DH_CTRL_REG3, 0x40); //打开INT1中断
+    lis3dh_write_reg(LIS3DH_CTRL_REG4, 0x00); //-2g~2g
+    lis3dh_write_reg(LIS3DH_CTRL_REG5, 0x00); //锁存INT1_SRC
+    lis3dh_write_reg(LIS3DH_CTRL_REG6, 0x00);
+
+    lis3dh_write_reg(LIS3DH_INT1_THS, 0x0B);
+    //设置阈值，2g单位为16mg，4g单位为32mg，8g单位为62mg，16g单位为186mg
+    lis3dh_write_reg(LIS3DH_INT1_DURATION, 0x00);   //设置唤醒时间，不需要进入睡眠
+    lis3dh_read_reg(LIS3DH_REFERENCE_REG, data, 2); //读取参考值寄存器，清除直流分量
+    lis3dh_write_reg(LIS3DH_INT1_CFG, 0x2A);        //当XYZ轴加速度高于阈值，产生中断
+
     return 0;
 }
