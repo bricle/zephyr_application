682f4d7f6422905b47ef8256a8696aab1133a8a9
Shan Pen <bricle031@gmail.com>
2025-08-04 22:19:29 +0800

    add rt spark
    Signed-off-by: Shan Pen <bricle031@gmail.com>

---
+++ boards/ruiside/stm32f407_rt_spark/board.cmake
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# keep first
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+board_runner_args(pyocd "--target=STM32F407ZGTx")
+
+# keep first
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
